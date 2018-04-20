8e15a1a12f6efaded910d623262a99618bb5b9b6
Michael Boroski <mjboroski@gmail.com>
2018-04-20 22:31:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,15 @@
 def prime?(n)
-  if n ≤ 1
+  if n <= 1
           return false
-  elsif n ≤ 3
+  elsif n <= 3
           return true
-  elsif n%2 = 0 or n%3 = 0
+  elsif n%2 == 0 or n%3 == 0
           return false
   end
 
   i = 5
-  while i * i ≤ n
-    if n % i = 0 or n % (i + 2) = 0
+  while i * i <= n
+    if n % i == 0 or n % (i + 2) == 0
         return false
     end
     i = i + 6
